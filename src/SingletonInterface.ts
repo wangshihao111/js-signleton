c092acf95a36a80c7ef348c3b5bffbeca2965ab2
shihao wang <913003120@qq.com>
2021-11-05 15:46:17 +0800

    feat: 合并文件，升级依赖

--- src/SingletonInterface.ts
+++
@@ -1,3 +0,0 @@
-export interface SingletonInterface {
-  singleton: <T = any>(name: string, value: T) => T;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,48 @@
-import { GlobalSingleton } from "./GlobalSingleton";
-import { Singleton } from "./Singleton";
+export interface SingletonInterface {
+  singleton: <T = any>(name: string, value: T) => T;
+}
+
+export class Singleton implements SingletonInterface {
+  private pool: Map<any, any>;
+
+  constructor() {
+    this.pool = new Map();
+  }
+
+  public singleton(key: string, value: any) {
+    let singletonValue = this.pool.get(key);
+    if (!singletonValue) {
+      singletonValue = value;
+      this.pool.set(key, singletonValue);
+    }
+    return singletonValue;
+  }
+}
+
+export class GlobalSingleton implements SingletonInterface {
+  private pool: Record<string, any>;
+
+  constructor() {
+    const g: any =
+      typeof window !== 'undefined'
+        ? window
+        : typeof global !== 'undefined'
+        ? global
+        : globalThis;
+    if (g.__jsSingleton__) {
+      this.pool = g.__jsSingleton__;
+    } else {
+      this.pool = g.__jsSingleton__ = {};
+    }
+  }
+  singleton(key: string, value: any) {
+    let singletonValue = this.pool[key];
+    if (!singletonValue) {
+      singletonValue = this.pool[key] = value;
+    }
+    return singletonValue;
+  }
+}
 
 const singleton = new Singleton();
 
@@ -12,3 +55,5 @@ export function createSingleton<T = any>(key: string, value: T): T {
 export function createGlobalSingleton<T = any>(key: string, value: T): T {
   return globalSingleton.singleton(key, value);
 }
+
+export default createGlobalSingleton;
